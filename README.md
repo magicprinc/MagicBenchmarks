b3fe9483889214b301b9f9ea4c20239ca40b85ec
A.Fink <andrey.fink@platform-s.com>
2023-12-08 01:53:43 +0300

    Compare Jackson Base64 and Vertx/JDK Jackson Base64

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # benchmarks4vertx
+
+## Jackson Base64 _-vs-_ JDK Base64
+```
+JMH 1.37
+JDK 17.0.9, OpenJDK 64-Bit Server VM, 17.0.9+11-LTS
+JDK 21.0.1, OpenJDK 64-Bit Server VM, 21.0.1+12-LTS
+Jackson 2.16.0
+```
+### JDK 17
+```
+Benchmark                                       Mode  Cnt   Score   Error  Units
+JacksonBase64.testDefaultJacksonBase64         thrpt   10  21,129 ± 0,551  ops/s
+JacksonBase64.testDefaultJacksonBase64AndBack  thrpt   10  10,315 ± 0,146  ops/s
+JacksonBase64.testVertxBase64                  thrpt   10  20,442 ± 2,851  ops/s
+JacksonBase64.testVertxBase64AndBack           thrpt   10   7,574 ± 0,210  ops/s
+```
+### JDK 21
+```
+Benchmark                                       Mode  Cnt   Score   Error  Units
+JacksonBase64.testDefaultJacksonBase64         thrpt   10  23,342 ± 0,325  ops/s
+JacksonBase64.testDefaultJacksonBase64AndBack  thrpt   10  10,967 ± 0,217  ops/s
+JacksonBase64.testVertxBase64                  thrpt   10  23,784 ± 0,906  ops/s
+JacksonBase64.testVertxBase64AndBack           thrpt   10  10,135 ± 0,068  ops/s
+```
+### JDK 17 jackson-module-blackbird
+```
+Benchmark                                       Mode  Cnt   Score   Error  Units
+JacksonBase64.testDefaultJacksonBase64         thrpt   10  24,628 ± 1,056  ops/s
+JacksonBase64.testDefaultJacksonBase64AndBack  thrpt   10  10,471 ± 0,065  ops/s
+JacksonBase64.testVertxBase64                  thrpt   10  22,038 ± 0,247  ops/s
+JacksonBase64.testVertxBase64AndBack           thrpt   10   7,917 ± 0,050  ops/s
+```
+### JDK 21 jackson-module-blackbird
+```
+Benchmark                                       Mode  Cnt   Score   Error  Units
+JacksonBase64.testDefaultJacksonBase64         thrpt   10  22,977 ± 0,291  ops/s
+JacksonBase64.testDefaultJacksonBase64AndBack  thrpt   10  11,243 ± 0,044  ops/s
+JacksonBase64.testVertxBase64                  thrpt   10  24,319 ± 0,205  ops/s
+JacksonBase64.testVertxBase64AndBack           thrpt   10  10,066 ± 0,115  ops/s
+```
---
+++ src/main/java/benchmark/JacksonBase64.java
@@ -0,0 +1,145 @@
+package benchmark;
+
+import com.fasterxml.jackson.core.Base64Variants;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Preconditions;
+import io.smallrye.mutiny.unchecked.UncheckedConsumer;
+import io.vertx.core.json.jackson.DatabindCodec;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+import java.util.stream.IntStream;
+
+import static com.google.common.base.Verify.verify;
+
+/**
+ Jackson Base64
+ -vs-
+ Vert.x/JDK Base64
+
+ @see DatabindCodec#mapper
+ @see org.openjdk.jmh.annotations.Benchmark
+ */
+@Warmup(iterations = 20, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 10, time = 2, timeUnit = TimeUnit.SECONDS)
+@Threads(1)
+@BenchmarkMode(Mode.Throughput) // Mode.AverageTime
+@Fork(value = 1, jvmArgs = {
+	"-XX:+UseParallelGC", "-XX:+UseCompressedOops", "-XX:+DoEscapeAnalysis", "-Xmx4G"
+	//, "-XX:CompileCommand=inline,java/lang/String.charAt"
+})
+@OutputTimeUnit(TimeUnit.SECONDS)
+@State(Scope.Thread)
+@Slf4j
+public class JacksonBase64 {
+
+	static final List<Map<String,byte[]>> SAMPLES = IntStream.range(50_000, 50_100).mapToObj(i->{
+			var bytes = new byte[i*4];
+			ThreadLocalRandom.current().nextBytes(bytes);
+			return Map.of(Integer.toString(i), bytes);
+		}).toList();
+
+	static final TypeReference<Map<String,byte[]>> MAP_TYPE = new TypeReference<>(){};
+
+	static final ObjectMapper mapper = new ObjectMapper();
+	static {
+		mapper.configure(JsonParser.Feature.ALLOW_COMMENTS, true);
+		mapper.setBase64Variant(Base64Variants.MODIFIED_FOR_URL);// -_ instead of +/
+		mapper.findAndRegisterModules();// for blackbird
+		DatabindCodec.mapper().findAndRegisterModules();
+	}
+
+	public static <T> Consumer<T> consume (@NonNull UncheckedConsumer<T> lambda) {
+		return lambda.toConsumer();
+	}
+
+	@Benchmark
+	public void testVertxBase64 () {
+		SAMPLES.forEach(consume(map->{
+			String s = DatabindCodec.mapper().writeValueAsString(map);
+			verify(s.length() > 100);
+		}));
+	}
+
+	@Benchmark
+	public void testDefaultJacksonBase64 () {
+		SAMPLES.forEach(consume(map->{
+			String s = mapper.writeValueAsString(map);
+			verify(s.length() > 100);
+		}));
+	}
+
+	@Benchmark
+	public void testVertxBase64AndBack () {
+		SAMPLES.forEach(consume(map->{
+			var s = DatabindCodec.mapper().writeValueAsString(map);
+			var map2 = DatabindCodec.mapper().readValue(s, MAP_TYPE);
+			verify((map.size() == 1) && (map2.size() == 1));
+			verify(Arrays.equals(map.values().iterator().next(), map2.values().iterator().next()));
+		}));
+	}
+
+	@Benchmark
+	public void testDefaultJacksonBase64AndBack () {
+		SAMPLES.forEach(consume(map->{
+			var s = mapper.writeValueAsString(map);
+			var map2 = mapper.readValue(s, MAP_TYPE);
+			verify((map.size() == 1) && (map2.size() == 1));
+			verify(Arrays.equals(map.values().iterator().next(), map2.values().iterator().next()));
+		}));
+	}
+
+	public static void main (String[] args) throws RunnerException, JsonProcessingException {
+		SAMPLES.forEach(consume(map->
+			Preconditions.checkState(Objects.equals(
+					DatabindCodec.mapper().writeValueAsString(map),
+					mapper.writeValueAsString(map)
+			))));
+		LOGGER.info("Same Json!");
+
+		Options opt = new OptionsBuilder()
+			.include(JacksonBase64.class.getSimpleName())
+			.build();
+
+		new Runner(opt).run();
+/*
+Options opt = new OptionsBuilder()
+              .include(this.getClass().getName() + ".*")
+              .mode (Mode.AverageTime)
+              .timeUnit(TimeUnit.MICROSECONDS)
+              .warmupTime(TimeValue.seconds(1))
+              .warmupIterations(2)
+              .measurementTime(TimeValue.seconds(1))
+              .measurementIterations(2)
+              .threads(2)
+              .forks(1)
+              .shouldFailOnError(true)
+              .shouldDoGC(true)
+              .build();
+          new Runner(opt).run(); */
+	}
+}
